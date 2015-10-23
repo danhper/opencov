79b4374acac7e654999ccab269dafa5c9f6d00a6
Daniel Perez <daniel@claudetech.com>
2015-10-23 22:39:08 +0900

    Add tests for find_by_token.

--- test/models/project_test.exs
+++ test/models/project_test.exs
@@ -15,4 +15,13 @@ defmodule Opencov.ProjectTest do
     changeset = Project.changeset(%Project{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "find_by_token with existing token" do
+    project = Repo.insert! Project.changeset(%Project{}, @valid_attrs)
+    assert Project.find_by_token(project.token) == project
+  end
+
+  test "find_by_token with inexisting token" do
+    assert Project.find_by_token("inexisting") == nil
+  end
 end
