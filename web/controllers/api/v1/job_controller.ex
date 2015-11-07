26fb29390da244e7a2d2d35ccf339aad7679a452
Daniel Perez <daniel@claudetech.com>
2015-11-07 11:21:46 +0900

    Fix job_controller API to be fully compatible with coveralls.

--- web/controllers/api/v1/job_controller.ex
+++ web/controllers/api/v1/job_controller.ex
@@ -1,16 +1,34 @@
 defmodule Api.V1.JobController do
   use Opencov.Web, :controller
 
-  def create(conn, %{"repo_token" => token} = params) do
-    project = Opencov.Project.find_by_token!(token)
-    {_, job} = Opencov.Project.add_job!(project, params)
-    IO.inspect(job)
-    render conn, "show.json", job: job
+  def create(conn, %{"json" => json}) do
+    json = Poison.decode!(json)
+    handle_create(conn, json)
+  end
+
+  def create(conn, %{"json_file" => %Plug.Upload{path: filepath}}) do
+    json = filepath |> File.read! |> Poison.decode!
+    IO.inspect(json)
+    handle_create(conn, json)
   end
 
   def create(conn, _) do
+    conn |> bad_request("request should have 'json' or 'json_file' parameter")
+  end
+
+  defp handle_create(conn, %{"repo_token" => token} = params) do
+    project = Opencov.Project.find_by_token!(token)
+    {_, job} = Opencov.Project.add_job!(project, params)
+    render conn, "show.json", job: job
+  end
+
+  defp handle_create(conn, _) do
+    conn |> bad_request("missing 'repo_token' parameter")
+  end
+
+  defp bad_request(conn, message) do
     conn
       |> put_status(400)
-      |> json %{"error" => "missing 'repo_token' parameter"}
+      |> json %{"error" => message}
   end
 end
