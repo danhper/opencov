1e7a608a94a40a5229c3ecf4b212c78b3aa6facb
Daniel Perez <daniel@claudetech.com>
2015-11-11 12:28:27 +0900

    Modify webpack config to change bootstrap theme.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,6 +2,8 @@ import riot from 'riot'
 
 import '../../../deps/phoenix_html/priv/static/phoenix_html'
 
+import '../css/main.styl'
+
 import './components'
 
 riot.mount('*')
--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,18 @@ var nib = require('nib')
 module.exports = {
   entry: {
     app: './web/static/js/app.js',
+    theme: './web/static/css/theme.less',
     vendor: [
       'jquery',
       'lodash',
       'highlight.js',
       'bootstrap',
-      'bootstrap/dist/css/bootstrap.css',
       'highlight.js/styles/solarized_light.css'
     ]
   },
   output: {
     path: './priv/static/js',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   devtool: 'source-map',
   module: {
@@ -25,6 +25,7 @@ module.exports = {
       {test: /\.js$/, loader: 'babel?optional[]=runtime', include: /web\/static\/js/},
       {test: /\.jade$/, loader: 'jade'},
       {test: /\.styl$/, loader: ExtractTextPlugin.extract('style-loader', 'css!stylus')},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css!less')},
       {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
       {test: /\.(png|woff|woff2|eot|ttf|svg|gif)/, loader: 'url-loader?limit=10000'},
       {test: /\.jpg/, loader: 'file-loader'}
@@ -32,7 +33,6 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('[name].css', {allChunks: true}),
-    new webpack.optimize.CommonsChunkPlugin('vendor', '[name].bundle.js'),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
