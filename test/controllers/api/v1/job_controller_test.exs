3ff6c62c66ff033692adc3743f1934f01444bd90
Daniel Perez <daniel@claudetech.com>
2015-12-02 11:59:13 +0900

    Add test for job API multipart upload.

--- test/controllers/api/v1/job_controller_test.exs
+++ test/controllers/api/v1/job_controller_test.exs
@@ -6,6 +6,11 @@ defmodule Opencov.Api.V1.JobControllerTest do
     {:ok, conn: conn}
   end
 
+  test "returns 400 when data not sent", %{conn: conn} do
+    conn = post conn, api_v1_job_path(conn, :create), ""
+    assert json_response(conn, 400)
+  end
+
   test "returns 400 when project token not given", %{conn: conn} do
     payload = Poison.encode!(%{json: Poison.encode!(Opencov.Fixtures.dummy_coverage)})
     conn = post conn, api_v1_job_path(conn, :create), payload
@@ -31,5 +36,17 @@ defmodule Opencov.Api.V1.JobControllerTest do
     job = List.first(Opencov.Repo.preload(build, :jobs).jobs)
     assert job
     assert job.files_count == Enum.count(data["source_files"])
+  end
+
+  test "works with multipart data", %{conn: conn} do
+    project = create(:project)
+    data = Dict.put(Opencov.Fixtures.dummy_coverage, "repo_token", project.token)
+    {:ok, file_path} = Temp.open %{prefix: "opencov", suffix: ".json"}, &IO.write(&1, Poison.encode!(data))
+    upload = %Plug.Upload{path: file_path, filename: "coverage.json", content_type: "application/json"}
+    conn = put_req_header(conn, "content-type", "multipart/form-data")
+    conn = post conn, api_v1_job_path(conn, :create), %{json_file: upload}
+    assert json_response(conn, 200)
+    assert Opencov.Build.for_commit(project, data["git"])
+    File.rm! file_path
   end
 end
