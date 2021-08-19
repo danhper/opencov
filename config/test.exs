0f8784691fbbe947323854fed8980b68cdd2c33d
Ale Ornelas <me@ale.world>
2021-08-19 18:07:18 -0500

    ci: add dummy key for tests

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,9 @@ config :opencov, Opencov.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-config :excoveralls, :endpoint, System.get_env("COVERALLS_ENDPOINT") || "http://b13e7de34700.ngrok.io"
+config :excoveralls,
+       :endpoint,
+       System.get_env("COVERALLS_ENDPOINT") || "http://b13e7de34700.ngrok.io"
 
 config :comeonin,
   bcrypt_log_rounds: 4,
@@ -23,3 +25,19 @@ config :opencov, Opencov.Repo,
   database: "opencov_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :joken,
+  rs256: [
+    signer_alg: "RS256",
+    key_pem: """
+    -----BEGIN RSA PRIVATE KEY-----
+    MIIBOQIBAAJAeDkTVci2/P3lt1yFWzB9YeoPFKmbp8aGAKjs8pZesurawbmo3QIk
+    DGQmrTmTjDDTbhGRyvuTn7ggB2p62F0nLQIDAQABAkASMLCYPjJRvSjQwZL75S5T
+    blKx0afXjtYfq2+OlOnnMCNXJ5jtk6nqfI/Nv0hYuieXeIWayfC1rrWxmng9Ym6R
+    AiEA20VMxxeSlASh6M6N+Tg4EytzIHjEhNvylkHCdSIil2MCIQCMXHCY7/d5xbT2
+    3Y7XJvxbbeKSSADa7yiw5wHrsTP0LwIhAK1j74LAMTi7MQ1XyQz6V91Qzoku9rfY
+    9cu71HmrtI7hAiB/C8z/IXWnS0UZjkGUjT0upK/IKFRd1svGE9KxO5wDEQIgUOv8
+    NnTX1NtsEv2tWklSraAadiqnVN1loGr2gKBZ9ZE=
+    -----END RSA PRIVATE KEY-----
+    """
+  ]
