6112df996262c98546551bcb9e68eb3db240d02b
Ale Ornelas <me@ale.world>
2021-08-19 16:28:49 -0500

    fix: iss swapped iat

--- lib/opencov/services/github_auth.ex
+++ lib/opencov/services/github_auth.ex
@@ -8,9 +8,9 @@ defmodule Opencov.Services.GithubAuth do
 
     token_config =
       %{}
-      |> Joken.Config.add_claim("iss", fn -> now - 60 end)
+      |> Joken.Config.add_claim("iat", fn -> now - 60 end)
       |> Joken.Config.add_claim("exp", fn -> now + 60 end)
-      |> Joken.Config.add_claim("iat", fn -> "133119" end)
+      |> Joken.Config.add_claim("iss", fn -> "133119" end)
 
     {:ok, jwt, _} = Joken.generate_and_sign(token_config, %{}, signer)
     jwt
