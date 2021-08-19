359eb59622932807ab78ab7a5a1913f6af54d038
Ale Ornelas <me@ale.world>
2021-08-19 09:04:05 -0500

    build(deps): upgrade elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Opencov.Mixfile do
   def project do
     [app: :opencov,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.12",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
