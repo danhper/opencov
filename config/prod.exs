ba83cdbf1f6652c7c65e5bfbbfe494c6d641b21c
Ale Ornelas <me@ale.world>
2021-08-22 15:51:15 -0500

    fix: different sentry config

--- config/prod.exs
+++ config/prod.exs
@@ -40,11 +40,13 @@ config Librecov.Plug.Github,
 
 config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
+  environment_name: :prod,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
   tags: %{
     env: "production"
-  }
+  },
+  included_environments: [:prod]
 
 config :logger, Sentry.LoggerBackend,
   level: :error,
